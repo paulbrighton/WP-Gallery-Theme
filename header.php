29f7111333609365bac546ad557b868230244fd8
Paul Brighton <brighton.paul@yahoo.com>
2020-03-11 15:26:19 +0000

    Added sidebar with widget for adding categories and the search bar is now functioning

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>PhotoGenik</title>
+  <title><?php bloginfo('title') ?></title>
   <meta charset="<?php bloginfo('charset') ?>">
 	<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
@@ -14,7 +14,9 @@
 		    <h1><?php bloginfo('name') ?></h1>
 		  </div>
 		  <div class="w3-col m3 l3">
-		    <input class="w3-input" type="text" placeholder="Search...">
+        <form action="<?php echo esc_url(home_url('/')); ?>" method="get">
+          <input name="s" class="w3-input" type="text" placeholder="Search...">
+        </form>
 		  </div>
 		</div>
 	</header>
@@ -22,13 +24,9 @@
 	<div class="main">
 		<div class="w3-row main-section">
 			<div class="w3-col m3 l3">
-			<ul class="w3-ul">
-				<li><a href="#">Nature</a></li>
-				<li><a href="#">Animals</a></li>
-				<li><a href="#">Objects</a></li>
-				<li><a href="#">People</a></li>
-				<li><a href="#">Abstract</a></li>
-			</ul>
+        <?php if(is_active_sidebar('sidebar')) : ?>
+          <?php dynamic_sidebar('sidebar') ?>
+        <?php endif; ?>
 			</div>
 			<div class="w3-col m9 l9">
         <div class="w3-row">
